42024ad8bfc589c28ee01bc9b0dc97f946b32d7c
lea <leaprovenzano@gmail.com>
2020-12-14 18:09:18 +0000

    add test for len on sampler

--- tests/test_sampler/test_subsequence_sampler.py
+++ tests/test_sampler/test_subsequence_sampler.py
@@ -1,5 +1,6 @@
 from hearth.samplers import SubsequenceSampler
 from itertools import chain
+import pytest
 
 
 def test_subsequence_sampler():
@@ -13,3 +14,14 @@ def test_subsequence_sampler():
     assert len(batches) == len(sampler)
     assert sum(map(lambda x: len(x) == 12, batches)) == 8
     assert sorted(chain.from_iterable(batches)) == list(range(100))
+
+
+@pytest.mark.parametrize(
+    'sampler, expected',
+    [
+        (SubsequenceSampler(range(100), batch_size=12), 9),
+        (SubsequenceSampler(range(100), batch_size=12, drop_shortest=True), 8),
+    ],
+)
+def test_len(sampler, expected):
+    assert len(sampler) == expected
