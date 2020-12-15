1830f396b3bdcf83815c1ef86c446f9bb0ce4bbc
lea <leaprovenzano@gmail.com>
2020-12-15 10:06:52 +0000

    add registry for getting activations by name.

--- src/hearth/activations.py
+++ src/hearth/activations.py
@@ -1,5 +1,31 @@
+from typing import Type
 import torch
 from torch import nn
+from hearth._internals import Registry
+
+_activation_registry: Registry[Type[nn.Module]] = Registry('activations')
+_activation_registry.register(nn.ReLU)
+_activation_registry.register(nn.RReLU)
+_activation_registry.register(nn.Hardtanh)
+_activation_registry.register(nn.ReLU6)
+_activation_registry.register(nn.Sigmoid)
+_activation_registry.register(nn.Hardsigmoid)
+_activation_registry.register(nn.Tanh)
+_activation_registry.register(nn.SiLU)
+_activation_registry.register(nn.Hardswish)
+_activation_registry.register(nn.ELU)
+_activation_registry.register(nn.CELU)
+_activation_registry.register(nn.SELU)
+_activation_registry.register(nn.GLU)
+_activation_registry.register(nn.GELU)
+_activation_registry.register(nn.Hardshrink)
+_activation_registry.register(nn.LeakyReLU)
+_activation_registry.register(nn.LogSigmoid)
+_activation_registry.register(nn.Softplus)
+_activation_registry.register(nn.Softshrink)
+_activation_registry.register(nn.PReLU)
+_activation_registry.register(nn.Softsign)
+_activation_registry.register(nn.Tanhshrink)
 
 
 def mish(x: torch.Tensor) -> torch.Tensor:
@@ -7,6 +33,7 @@ def mish(x: torch.Tensor) -> torch.Tensor:
     return x * torch.tanh(torch.nn.functional.softplus(x))
 
 
+@_activation_registry.register
 class Mish(nn.Module):
     """Applies the Mish activation function element-wise
 
@@ -30,3 +57,36 @@ class Mish(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return mish(x)
+
+
+def get_activation(name: str, **kwargs) -> nn.Module:
+    """get an new instance activiation by its name, optionally passing any extra kwargs.
+
+    Args:
+        name: string name of the activation. will be best effort normalized.
+
+    Returns:
+        nn.Module: instance of an activation module
+
+    Example:
+        >>> from torch import nn
+        >>> from hearth.activations import get_activation
+        >>>
+        >>> get_activation('relu')
+        ReLU()
+
+        you can also pass kwargs... although this is slightly silly
+        since you may as well use the actual module then...
+
+        >>> get_activation('prelu', num_parameters=6)
+        PReLU(num_parameters=6)
+
+        name will be normalized so dont worry about casing:
+
+        >>> get_activation('mish')
+        Mish()
+
+        >>> get_activation('MISH')
+        Mish()
+    """
+    return _activation_registry[name](**kwargs)  # type: ignore
