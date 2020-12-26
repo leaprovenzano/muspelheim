6a26c4b7e4b1a5164770c265da69cede2ff14131
lea <leaprovenzano@gmail.com>
2020-12-26 07:09:59 +0000

    add trainable_parameters helper

--- src/hearth/grad.py
+++ src/hearth/grad.py
@@ -1,3 +1,4 @@
+from typing import Iterator
 from torch import nn
 
 
@@ -19,3 +20,13 @@ def unfreeze(model: nn.Module):
     """
     for param in model.parameters():
         param.requires_grad = True
+
+
+def requires_grad(param: nn.Parameter) -> bool:
+    """return true if the parameter requires grad"""
+    return param.requires_grad
+
+
+def trainable_parameters(model: nn.Module) -> Iterator[nn.Parameter]:
+    """yields trainable parameters from a model."""
+    yield from filter(requires_grad, model.parameters())
--- tests/test_grad/test_freeze_unfreeze.py
+++ tests/test_grad/test_freeze_unfreeze.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 import pytest
-from hearth.grad import freeze, unfreeze
+from hearth.grad import freeze, unfreeze, trainable_parameters
 
 
 def py_model():
@@ -13,8 +13,18 @@ def script_model():
     return torch.jit.script(model)
 
 
+def partially_frozen_model(freeze_n: int = 1):
+    model = nn.Sequential(nn.Linear(1, 2), nn.Linear(2, 3), nn.Linear(3, 3))
+    for i in range(freeze_n):
+        freeze(model[i])
+        assert not model[i].weight.requires_grad
+        assert not model[i].bias.requires_grad
+    return model
+
+
 @pytest.mark.parametrize(
-    'model,', [py_model(), script_model()],
+    'model,',
+    [py_model(), script_model()],
 )
 def test_freeze(model):
     freeze(model)
@@ -23,10 +33,20 @@ def test_freeze(model):
 
 
 @pytest.mark.parametrize(
-    'model,', [py_model(), script_model()],
+    'model,',
+    [py_model(), script_model()],
 )
 def test_unfreeze(model):
     freeze(model)
     unfreeze(model)
     for param in model.parameters():
+        assert param.requires_grad
+
+
+@pytest.mark.parametrize(
+    'model,',
+    [py_model(), partially_frozen_model(1), partially_frozen_model(2)],
+)
+def test_trainable_params(model):
+    for param in trainable_parameters(model):
         assert param.requires_grad
